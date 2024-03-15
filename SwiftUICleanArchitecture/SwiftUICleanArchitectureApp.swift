3647a104684d7efeacb3e5b6879076c62bf3747d
martin-ngigi <martinwainaina001@gmail.com>
2024-03-15 12:15:56 +0300

    Initial commit

---
+++ SwiftUICleanArchitecture/SwiftUICleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanArchitectureApp.swift
+//  SwiftUICleanArchitecture
+//
+//  Created by Martin Wainaina on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
